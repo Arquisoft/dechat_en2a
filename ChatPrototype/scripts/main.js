94520adcd74368378f37ab5939be2e3c186a4923
Yeray <yeray698@hotmail.com>
2019-02-23 11:31:47 +0100

    Storage improvements to the prototype
    In order to make dealing with message files easier from now on, saving and loading chat logs is done in JSON format instead of manually parsing text files (which are still there just in case). Retrieving the chat  now doesn't show duplicate messages twice.

--- ChatPrototype/scripts/main.js
+++ ChatPrototype/scripts/main.js
@@ -2,7 +2,7 @@ const FOAF = $rdf.Namespace('http://xmlns.com/foaf/0.1/');
 
 // Log the user in and out on click
 const popupUri = 'popup.html';
-$('#login  button').click(() => solid.auth.popupLogin({ popupUri }));
+$('#login  button').click(() => solid.auth.popupLogin({  popupUri }));
 $('#logout button').click(() => solid.auth.logout());
 
 // Update components to match the user's login status
@@ -15,8 +15,8 @@ solid.auth.trackSession(session => {
     // Use the user's WebID as default profile
     if (!$('#profile').val())
       $('#profile').val(session.webId);
-      if (!$('#public').val())
-      $('#public').val(String(session.webId).replace(/profile.*$/,"")+"public");
+    if (!$('#public').val())
+      $('#public').val(String(session.webId).replace(/profile.*$/, "") + "public");
 
     loadProfile();
   }
@@ -25,51 +25,77 @@ solid.auth.trackSession(session => {
 /**
  * Event function to create a new file
  */
-async function createOwnFile(){
+async function createOwnFile() {
   //creates a new empty chat file
-  SolidFileClient.updateFile($('#public').val()+'/chat01.txt','');
+  emptyChat($('#public').val() + '/chat01.json');
+  emptyChat($('#public').val() + '/chat01.txt'); //just in case
   //clears the screen messages
   $(".chat_container").empty();
 }
 $('#create').click(createOwnFile);
 
 /**
+ * Creates a new chat object and stores it in JSON format
+ * 
+ * @param {*} fileName 
+ */
+function emptyChat(fileName) {
+  var emptyChat = new Object();
+  emptyChat.chatNo = 1;
+  emptyChat.messages = new Array();
+  SolidFileClient.updateFile(fileName, JSON.stringify(emptyChat));
+}
+
+/**
  * Event function to send the message.
  */
-async function send(){
-  var texto=$("#content").val(); //get the text from the input box
-  if(texto != ""){ //if there there are messages to send
-    SolidFileClient.readFile($('#public').val()+'/chat01.txt').then(function(value) {
-      //read the file, and update it with the nex messages
-      SolidFileClient.updateFile($('#public').val()+'/chat01.txt',value + texto+"\n");
+async function send() {
+  var texto = $("#content").val(); //get the text from the input box
+  if (texto != "") { //if there there are messages to send
+    SolidFileClient.readFile($('#public').val() + '/chat01.json').then(function (value) {
+      var chat = JSON.parse(value); //Parse the content
+      chat.messages.push(getMessageObject(texto)); //Append the new message
+      SolidFileClient.updateFile($('#public').val() + '/chat01.json', JSON.stringify(chat)); //Store it
+      SolidFileClient.updateFile($('#public').val() + '/chat01.txt', JSON.stringify(chat)); //testing purposes
     });
     //append the message to the chat pannel
-    $(".chat_container" ).append( generateMessage(texto));
+    $(".chat_container").append(generateMessage(texto));
     //set the input box to blank
     $("#content").val("");
-  }          
+  }
 }
 $('#enviar').click(send);
+
+/**
+ * Creates a new message object from a specified content
+ * 
+ * @param {*} contents the content of the message
+ */
+function getMessageObject(contents) {
+  var messageObj = new Object();
+  messageObj.message = contents;
+  messageObj.timestamp = new Date().toLocaleString();
+  return messageObj;
+}
 
 /**
  * Auxiliary method to generate the messages in html
  * @param {*} text the message to be created
  */
-function generateMessage(text){
-  return "<div class=\"container\"><p>"+text+"</p></div>"
+function generateMessage(text) {
+  return "<div class=\"container\"><p>" + text + "</p></div>"
 }
 
 /**
  * Event function to retrieve the messages from the file
  */
-async function readChatFile(){
-  SolidFileClient.readFile($('#public').val()+'/chat01.txt').then(function(value) {
+async function readChatFile() {
+  SolidFileClient.readFile($('#public').val()+'/chat01.json').then(function(value) {
     $("#content").val(""); //empty the text area
-    value.split("\n").forEach(function(line){
-      //for each of the lines in the find, create a chat message.
-      if(line != ""){
-        $(".chat_container" ).append( generateMessage(line) );
-      }
+    $(".chat_container").empty(); //dont show same messages twice
+    var chat = JSON.parse(value);
+    chat.messages.forEach(function(msg) {
+      $(".chat_container" ).append( generateMessage(msg.message) );
     });
   });
 }
@@ -100,8 +126,8 @@ async function loadProfile() {
     $('#friends').append(
       $('<li>').append(
         $('<a>').text(fullName && fullName.value || friend.value)
-                .click(() => $('#profile').val(friend.value))
-                .click(loadProfile)));
+          .click(() => $('#profile').val(friend.value))
+          .click(loadProfile)));
   });
 }
 $('#view').click(loadProfile);
