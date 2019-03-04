f5876fd52dc0ccdbfbd1c834867a2d5ded362b10
Jose Manuel Estrada-Nora Muñoz <jotajotanora@gmail.com>
2019-03-04 21:04:41 +0100

    Fixing codacy errors

--- patch.js
+++ patch.js
@@ -1,28 +1,28 @@
-const fs = require('fs');
-const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+const fs = require("fs");
+const f = "node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js";
 
-fs.readFile(f, 'utf8', function (err,data) {
+fs.readFile(f, "utf8", function (err,data) {
   if (err) {
-    return console.log(err);
+    return
   }
-  var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true, fs: \'empty\'}');
+  var result = data.replace(/node: false/g, "node: {crypto: true, stream: true, fs: \'empty\'}");
 
-  fs.writeFile(f, result, 'utf8', function (err) {
-    if (err) return console.log(err);
+  fs.writeFile(f, result, "utf8", function (err) {
+    if (err) return;
   });
 });
 
 
 
-const f2 = 'node_modules/@comunica/actor-init-sparql/lib/ActorInitSparql-browser.js';
+const f2 = "node_modules/@comunica/actor-init-sparql/lib/ActorInitSparql-browser.js";
 
-fs.readFile(f2, 'utf8', function (err,data) {
+fs.readFile(f2, "utf8", function (err,data) {
   if (err) {
-    return console.log(err);
+    return;
   }
-  var result = data.replace(/operation = (await this.mediatorOptimizeQueryOperation.mediate({ context, operation })).operation;/g, '//operation = (await this.mediatorOptimizeQueryOperation.mediate({ context, operation })).operation;');
+  var result = data.replace(/operation = (await this.mediatorOptimizeQueryOperation.mediate({ context, operation })).operation;/g, "//operation = (await this.mediatorOptimizeQueryOperation.mediate({ context, operation })).operation;");
 
-  fs.writeFile(f, result, 'utf8', function (err) {
-    if (err) return console.log(err);
+  fs.writeFile(f, result, "utf8", function (err) {
+    if (err) return;
   });
 });
