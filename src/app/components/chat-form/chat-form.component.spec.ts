f4e774097d9cc722eaf8942d915d405e2855a452
Yeray <yeray698@hotmail.com>
2019-03-30 22:22:02 +0100

    Created some message sending tests

--- src/app/components/chat-form/chat-form.component.spec.ts
+++ src/app/components/chat-form/chat-form.component.spec.ts
@@ -3,7 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChatFormComponent } from './chat-form.component';
 import { FormsModule } from '@angular/forms';
 import { ChatService } from '../../services/chat.service';
+import { RdfService } from '../../services/rdf.service';
 import { ToastrModule } from 'ngx-toastr';
+import { By } from '@angular/platform-browser';
 
 describe('ChatFormComponent', () => {
     let component: ChatFormComponent;
@@ -13,7 +15,7 @@ describe('ChatFormComponent', () => {
         TestBed.configureTestingModule({
             declarations: [ChatFormComponent],
             imports: [ FormsModule , ToastrModule.forRoot() ],
-            providers: [ ChatService ]
+            providers: [ ChatService, RdfService ]
         })
             .compileComponents();
     }));
@@ -27,4 +29,29 @@ describe('ChatFormComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    describe('Send message', () => {
+        it('calls the component send function', async(() => {
+            spyOn(component, "send");
+            component.message = "Hello from chat form test";
+            component.send();
+            expect(component.send).toHaveBeenCalledTimes(1);
+        }));
+
+        it('calls the service send function', async(() => {
+            const chatServiceStub: ChatService = fixture.debugElement.injector.get(ChatService);
+            spyOn(chatServiceStub, "sendMessage");
+            component.message = "Hello from chat form test to chat service";
+            component.send();
+            expect(chatServiceStub.sendMessage).toHaveBeenCalledTimes(1);
+        }));
+    });
+
+    it('send button should call the send function', async(() => {
+        spyOn(component, "send");
+        let button = fixture.debugElement.query(By.css('#send')).nativeElement;
+        component.message = "Hello";
+        button.click();
+        expect(component.send).toHaveBeenCalledTimes(1);
+    }));
 });
