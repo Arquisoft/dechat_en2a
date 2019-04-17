5e21638c4dcd79a7b20ecc6750ad77cf1dad23d5
Luis <UO257809@uniovi.es>
2019-04-17 16:55:23 +0200

    Customization component
    Created and linked in the navbar

---
+++ src/app/components/customization/customization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customization',
+  templateUrl: './customization.component.html',
+  styleUrls: ['./customization.component.css']
+})
+export class CustomizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
