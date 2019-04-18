0993c107f45579f88bb94aab8d50edf8dee07b73
YerayG <yeray698@hotmail.com>
2019-04-18 11:15:36 +0200

    Sending test also fixed
    Now it has to be adapted to the new app rework

--- e2e/src/pages/message.po.ts
+++ e2e/src/pages/message.po.ts
@@ -5,29 +5,29 @@ export class MessagePage {
     password: string = 'elSupereselnumero1!';
 
     async login() {
-        await browser.get('/');
+        await browser.driver.get('localhost:4200');
         await element(by.css('.login-select')).click();
         await element(by.xpath('(//div[@class="provider"])[2]')).click();
-        await element(by.css('#btn-go')).click().then(() => browser.sleep(1000));
+        await element(by.css('#btn-go')).click().then(() => browser.sleep(2000));
         await browser.driver.findElement(by.id('username')).sendKeys(this.userName);
         await browser.driver.findElement(by.id('password')).sendKeys(this.password);
         await browser.driver.findElement(by.id('login')).click();
-        return browser.get('/chat');
+        return browser.driver.get('localhost:4200/chat').then(() => browser.sleep(2000));
     }
 
     async selectFriend() {
-        await element(by.tagName('app-friend-item')).click();
+        return await browser.driver.findElement(by.tagName('app-friend-item')).click();
     }
 
     async writeMessage(msg: string) {
-        await element(by.css('.chatInput')).sendKeys(msg);
+        return await browser.driver.findElement(by.css('.chatInput')).sendKeys(msg);
     }
 
     async clickSend() {
-        await browser.driver.findElement(by.id('send')).click();
+        return await browser.driver.findElement(by.id('send')).click().then(() => browser.sleep(1000));
     }
 
     async isMessageSent() {
-        return (await browser.driver.findElement(by.css('.messageContent')).isDisplayed());
+        return await browser.driver.findElement(by.css('.messageContent')).getText();
     }
 }
